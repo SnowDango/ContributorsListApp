2b0ff712db9ccaa5e78f4a1d683f37752f2b8a44
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-02-26 14:54:36 +0900

    adjust design

---
+++ app/src/main/java/com/snowdango/yumemicodetest/YumemiCodeTestApp.kt
@@ -0,0 +1,11 @@
+package com.snowdango.yumemicodetest
+
+import android.app.Application
+
+class YumemiCodeTestApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
--- app/src/main/java/com/snowdango/yumemicodetest/view/ContributorListActivity.kt
+++ app/src/main/java/com/snowdango/yumemicodetest/view/ContributorListActivity.kt
@@ -1,9 +1,10 @@
-package com.snowdango.yumemicodetest
+package com.snowdango.yumemicodetest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.snowdango.yumemicodetest.R
 
-class MainActivity : AppCompatActivity() {
+class ContributorListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
