3377df290401d7af775d64814b298bdae9e91ca8
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-02-26 16:27:24 +0900

    gradle.propertiesを.gitignoreに追加

---
+++ app/src/main/java/com/snowdango/yumemicodetest/data/netwrok/githubapi/ContributorsApi.kt
@@ -0,0 +1,13 @@
+package com.snowdango.yumemicodetest.data.netwrok.githubapi
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+
+
+interface ContributorsApi {
+
+    @GET()
+    fun getContributor(
+
+    )
+}
